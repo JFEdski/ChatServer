ef8eb75d94d6f225ce3aae53832029b21a8746e0
JFEdski <jedelinski@gmail.com>
2023-10-18 19:59:17 -0600

    More Stretch Goal

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -61,4 +61,46 @@ router.post('/login', async function (req, res) {
   }
 })
 
+router.patch('/:id', async(req, res) => {
+  try {
+    let _id = req.params.id;
+    let owner = req.user.id;
+
+    console.log(_id);
+    console.log(owner);
+
+    let updatedInfo = req.body;
+
+    const updated = await User.findOneAndUpdate({ _id, owner }, updatedInfo, { new: true });
+
+    if (!updated)
+      throw new Error("Invalid Pizza/User Combination")
+
+    res.status(200).json({
+      message: `${updated._id} Updated!`,
+      updated
+    })
+} catch (err) {
+    errorResponse(res, err);
+}
+});
+
+
+router.delete('/:id', async (req, res) => {
+  try {
+  const { id } = req.params;
+  let owner = req.user.id;
+  const deletedPizza = await Pizza.deleteOne({ _id: id, owner });
+if (!deletedPizza.deletedCount) {
+  throw new Error('could not find pizza bud')
+} 
+  res.status(200).json({ 
+    message: 'Pizza Deleted!',
+    deletedPizza
+  });
+} catch (error) {
+  res.status(500).json({ error: error.message });
+}
+})
+
 module.exports = router;
--- index.js
+++ index.js
@@ -10,8 +10,10 @@ app.use(require('cors')());
 
 const users = require('./controllers/user.controller');
 const rooms = require('./controllers/room.controller');
-const validateSession = require('./middleware/validateSession');
+const validateSession = require('./middleware/validatesession');
 
 app.use('/user', users);
 app.use(validateSession);
 app.use('/room', rooms);
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
